907017cd775151fc92e7d911b34925c480a515a6
albindahlberg <albin.c.dahlberg@gmail.com>
2022-08-01 16:55:45 +0200

    typo fic

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,19 @@
 import Swarm from './swarm.js'
 
-// Setup canvas
 const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-// Setup boids
 const number_of_boids = 10
 const swarm = new Swarm(number_of_boids)
 swarm.spawn(ctx)
 
-// Main loop, animates indefinitely
+/**
+ * Main loop, runs the animation indefinitely 
+ */
 let animate = function() {
-  requestAnimationFrame(updateCanvas)
+  requestAnimationFrame(animate)
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   swarm.update(ctx)
 }
