a788332880a53aa14254b622404bffa2ff2c5f4f
albindahlberg <albin.c.dahlberg@gmail.com>
2022-08-01 16:52:11 +0200

    documentation for main loop

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,21 @@
 import Swarm from './swarm.js'
 
+// Setup canvas
 const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext('2d')
-
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-let swarm = new Swarm(10)
-
+// Setup boids
+const number_of_boids = 10
+const swarm = new Swarm(number_of_boids)
 swarm.spawn(ctx)
 
-let updateCanvas = function() {
+// Main loop, animates indefinitely
+let animate = function() {
   requestAnimationFrame(updateCanvas)
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   swarm.update(ctx)
 }
 
-
-updateCanvas()
+animate()
